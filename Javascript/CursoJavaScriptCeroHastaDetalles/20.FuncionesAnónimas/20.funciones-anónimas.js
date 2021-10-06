5d30c18463cdbafc2e15a9f7aa244be7e28340b9
Esneider1997 <esneidermanuelbuelvas1997@gmail.com>
2021-10-05 23:01:38 -0500

    20 funciones anonimas completar

---
+++ Javascript/CursoJavaScriptCeroHastaDetalles/20.FuncionesAnónimas/20.funciones-anónimas.js
@@ -0,0 +1,29 @@
+
+// Las funciones anonimas sirven para mantener nuestro codigo encapsulado y que no sea manipulado por otras seciones de nuestro proframa
+/* var a = 10;
+
+
+console.log( a );
+
+function cambiarA() {
+    a = 20;
+}
+
+cambiarA();
+
+console.log( a ); */
+
+// Funcion anomuna
+( function name(params) {
+    var a = 10;
+
+    console.log( a );
+
+    function cambiarA() {
+        a = 20;
+    }
+
+    cambiarA();
+
+    console.log( a );
+})();
--- Javascript/JavascriptCeroHastaDetalles/16.Metodos&ObjetoThis/16.Metodos&ObjetoThis.js
+++
@@ -1,9 +0,0 @@
-var persona = {
-    nombre:'Esneider',
-    apellido: 'Buelvas Olivo',
-    imprimirNombre: () => {
-        document.write('Imprimir nombre');
-    }
-}
-
-console.log(persona.imprimirNombre());

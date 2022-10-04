5c47bb3cb1a5dbecbcf4a0551618dfd7cb795ed2
Esneider M <esneidermanuelbuelvas1997@gmail.com>
2022-10-04 11:16:43 -0500

    Prueba de php

---
+++ PHP/1. PRUENAS/1. Talent[ly].php
@@ -0,0 +1,11 @@
+// Tipos de arreglo en PHP.
+
+// como hacer una coneccion.
+
+// Como inicializamos una session.
+
+// Cual es el tiempo por defecto para una ejecucion de un script en PHP  y como se puede modificar. NOTA: ini.php
+// R: 30 segundo 
+
+// diferencia entre die() y exit()
+  //La función die() imprime un mensaje y sale del script actual. Esta función es un alias de la función exit().
